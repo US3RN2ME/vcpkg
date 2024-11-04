6128617dc8f7a6053a6314bf35941d8086edce2d
Dmytro Tereshchenko <dmytro.tereshchenko@saboit.de>
2024-11-04 20:51:16 +0100

    [kquickcharts] Try last version

--- ports/kfquickcharts/portfile.cmake
+++ ports/kfquickcharts/portfile.cmake
@@ -1,10 +1,9 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO KDE/kquickcharts
-  REF v6.7.0
+  REF v6.8.0-rc1
   SHA512 9e8537e12d2f4f1f43a36aa0cc5406de25be190a12cbeb81b4811656b3a3b4489229b68a5cb85962bd540cc3295dcc220b27fe472bd551fad7133f7cce6939f0
   HEAD_REF master
-  PATCHES "fix-target-install.patch"
 )
 
 # Prevent KDEClangFormat from writing to source effectively blocking parallel configure
@@ -15,7 +14,7 @@ vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DBUILD_TESTING=OFF)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME KF6QuickCharts CONFIG_PATH lib/cmake/KF6QuickCharts)
 vcpkg_copy_pdbs()
-#f
+
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
   file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
