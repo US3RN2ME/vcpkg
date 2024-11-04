d0324300d3593a08f928e27a2345a1f605e713d8
Dmytro Tereshchenko <dmytro.tereshchenko@saboit.de>
2024-11-04 19:45:19 +0100

    [kquickcharts] Reset version

--- ports/kfquickcharts/portfile.cmake
+++ ports/kfquickcharts/portfile.cmake
@@ -1,14 +1,9 @@
 vcpkg_from_github(
-  OUT_SOURCE_PATH
-  SOURCE_PATH
-  REPO
-  KDE/kquickcharts
-  REF
-  v6.8.0-rc1
-  SHA512
-  fc18beefbb12526210bfd2b2f17522637897f1a920e2036d516a143fbcf0cf27fd8a7f47cfb2904177dcd98f1e7a330770cf101a8fda11728272127a5b5295d7
-  HEAD_REF
-  master
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO KDE/kquickcharts
+  REF v6.7.0
+  SHA512 0
+  HEAD_REF master
 )
 
 # Prevent KDEClangFormat from writing to source effectively blocking parallel configure
