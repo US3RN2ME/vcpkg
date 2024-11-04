dc019b4cd9ae745e9eabcc501b6c445dedf56c0d
Dmytro Tereshchenko <dmytro.tereshchenko@saboit.de>
2024-11-04 20:05:34 +0100

    [kquickcharts] Update config path

--- ports/kfquickcharts/portfile.cmake
+++ ports/kfquickcharts/portfile.cmake
@@ -13,6 +13,8 @@ vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DBUILD_TESTING=OFF)
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME KF6QuickCharts CONFIG_PATH lib/cmake/KF6QuickCharts)
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF6QuickCharts CONFIG_PATH share/KF6QuickCharts)
+
 vcpkg_copy_pdbs()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
