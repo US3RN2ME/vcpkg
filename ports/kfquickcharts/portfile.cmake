58bfd798b57bf41403ac063355dd687811864384
Dmytro Tereshchenko <dmytro.tereshchenko@saboit.de>
2024-11-04 20:08:05 +0100

    [kquickcharts] Update config path

--- ports/kfquickcharts/portfile.cmake
+++ ports/kfquickcharts/portfile.cmake
@@ -12,8 +12,8 @@ file(WRITE ${SOURCE_PATH}/.clang-format "DisableFormat: true\nSortIncludes: fals
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DBUILD_TESTING=OFF)
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME KF6QuickCharts CONFIG_PATH lib/cmake/KF6QuickCharts)
-vcpkg_cmake_config_fixup(PACKAGE_NAME KF6QuickCharts CONFIG_PATH share/KF6QuickCharts)
+#vcpkg_cmake_config_fixup(PACKAGE_NAME KF6QuickCharts CONFIG_PATH lib/cmake/KF6QuickCharts)
+vcpkg_cmake_config_fixup(PACKAGE_NAME KF6QuickCharts CONFIG_PATH ../share/KF6QuickCharts)
 
 vcpkg_copy_pdbs()
 
