ba642af55e071154282a27f15a28f6b41048e104
Dmytro Tereshchenko <dmytro.tereshchenko@saboit.de>
2024-11-04 20:54:58 +0100

    [kquickcharts] Update hash

--- ports/kfquickcharts/portfile.cmake
+++ ports/kfquickcharts/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO KDE/kquickcharts
-  REF v6.8.0-rc1
-  SHA512 fc18beefbb12526210bfd2b2f17522637897f1a920e2036d516a143fbcf0cf27fd8a7f47cfb2904177dcd98f1e7a330770cf101a8fda11728272127a5b5295d7
+  REF v6.7.0
+  SHA512 9e8537e12d2f4f1f43a36aa0cc5406de25be190a12cbeb81b4811656b3a3b4489229b68a5cb85962bd540cc3295dcc220b27fe472bd551fad7133f7cce6939f0
   HEAD_REF master
 )
 
@@ -14,7 +14,7 @@ vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DBUILD_TESTING=OFF)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME KF6QuickCharts CONFIG_PATH lib/cmake/KF6QuickCharts)
 vcpkg_copy_pdbs()
-
+#f
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
   file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
