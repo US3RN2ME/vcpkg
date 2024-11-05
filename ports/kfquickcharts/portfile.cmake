91988ceb2ddacbcaeb7106a3872be026e76d7614
Dmytro Tereshchenko <dmytro.tereshchenko@saboit.de>
2024-11-05 11:50:44 +0100

    [kquickcharts] Add patch

--- ports/kfquickcharts/portfile.cmake
+++ ports/kfquickcharts/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
   REF v6.7.0
   SHA512 9e8537e12d2f4f1f43a36aa0cc5406de25be190a12cbeb81b4811656b3a3b4489229b68a5cb85962bd540cc3295dcc220b27fe472bd551fad7133f7cce6939f0
   HEAD_REF master
+  PATCHES "fix-target-install.patch"
 )
 
 # Prevent KDEClangFormat from writing to source effectively blocking parallel configure
@@ -21,3 +22,5 @@ endif()
 
 file(GLOB LICENSE_FILES "${SOURCE_PATH}/LICENSES/*")
 vcpkg_install_copyright(FILE_LIST ${LICENSE_FILES})
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
